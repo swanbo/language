74637074a7ffe2b09471726b90691315942aef5b
swanbo <test@gmail.com>
2022-06-16 00:49:15 +0800

    ADD: 增加句子的数据模型; MOD: 修改admin页面的内容;

--- words/admin.py
+++ words/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 
 from .models import Word
+from .models import Sentence
 
-admin.site.register(Word)
+
+class SentenceInline(admin.StackedInline):
+    model = Sentence
+    fields = ('context',)
+
+
+class WordAdmin(admin.ModelAdmin):
+    list_display = ('spell', 'pron', 'zh', 'en', 'remark', 'create_date')
+    inlines = [SentenceInline]
+
+
+class SentenceAdmin(admin.ModelAdmin):
+    list_display = ('word', 'context', 'translation', 'create_date')
+
+
+admin.site.register(Word, WordAdmin)
+admin.site.register(Sentence, SentenceAdmin)
--- words/models.py
+++ words/models.py
@@ -3,11 +3,25 @@ from django.db import models
 
 class Word(models.Model):
     spell = models.CharField(max_length=40, verbose_name='拼写', unique=True)
+    prefix = models.CharField(max_length=40, verbose_name='前缀', null=True, blank=True)
+    suffix = models.CharField(max_length=40, verbose_name='后缀', null=True, blank=True)
     pron = models.CharField(max_length=40, verbose_name='音标', null=True, blank=True)
-    zh = models.CharField(max_length=40, verbose_name='汉译', null=True, blank=True)
-    en = models.CharField(max_length=40, verbose_name='英译', null=True, blank=True)
+    zh = models.CharField(max_length=400, verbose_name='汉译', null=True, blank=True)
+    en = models.CharField(max_length=400, verbose_name='英译', null=True, blank=True)
     remark = models.CharField(max_length=400, verbose_name='备注', null=True, blank=True)
-    create_date = models.DateTimeField(verbose_name='创建日期')
+    create_date = models.DateTimeField(verbose_name='创建日期', auto_now_add=True)
 
     def __str__(self):
         return self.spell
+
+
+class Sentence(models.Model):
+    """和单词是多对一的关系"""
+    word = models.ForeignKey('Word', on_delete=models.CASCADE)
+    context = models.CharField(max_length=400, verbose_name='句子', null=True, blank=True)
+    translation = models.CharField(max_length=400, verbose_name='译文', null=True, blank=True)
+    remark = models.CharField(max_length=400, verbose_name='备注', null=True, blank=True)
+    create_date = models.DateTimeField(verbose_name='创建日期', auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.context[:20]}......' if len(self.context) >= 30 else self.context

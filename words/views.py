6ce4e1a0305880d4e74f626e35da87afc6382f69
swanbo <test@gmail.com>
2022-07-01 00:34:15 +0800

    添加了简单的前端展示页面

--- words/views.py
+++ words/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from . import models
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    word_list = models.Word.objects.all()
+    return render(request, "words/index.html", locals())
+
+
+def index_temp(request):
+    word_list = models.Word.objects.all()
+    return render(request, "words/index_temp.html", locals())
